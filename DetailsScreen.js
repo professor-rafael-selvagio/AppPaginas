7ae2a806bb68c10c986e9f9a75cbdd9c46ea37d1
Professor Rafael Selvagio <professor.rafael.selvagio@gmail.com>
2024-02-25 22:32:26 -0300

    Primeiro Commit

---
+++ DetailsScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const DetailsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Detalhes</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default DetailsScreen;
+
---
+++ HomeScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Ver detalhes"
+        onPress={() => navigation.navigate('Detalhes')}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
